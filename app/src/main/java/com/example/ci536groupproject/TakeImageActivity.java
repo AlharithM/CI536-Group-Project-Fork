2ce251e919cbb4637b0c5a6e48aac37cd909a793
Oliver Conway <85183630+foxinbox4ever@users.noreply.github.com>
2024-03-01 15:13:19 +0000

    Update TakeImageActivity.java
    not finished yet

--- app/src/main/java/com/example/ci536groupproject/TakeImageActivity.java
+++ app/src/main/java/com/example/ci536groupproject/TakeImageActivity.java
@@ -6,9 +6,29 @@ import android.os.Bundle;
 
 public class TakeImageActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_image);
+        
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.takeImage);
+
+        if(ContextCompat.checkSelfPermission(TakeImageActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActvivityCompat.requestPermissions(TakeImageActivity.this, new Sting[]{
+                Manifest.permission.CAMERA
+            }, 100);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });https://www.youtube.com/watch?v=XRD-lVwlSjU
     }
 }
