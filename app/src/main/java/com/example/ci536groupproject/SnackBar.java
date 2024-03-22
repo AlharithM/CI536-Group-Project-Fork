dd787dfa4b10c66e84c75bffd70860dc8ab9f2c9
Oliver Conway <85183630+foxinbox4ever@users.noreply.github.com>
2024-03-22 14:38:16 +0000

    Update SnackBar.java

--- app/src/main/java/com/example/ci536groupproject/SnackBar.java
+++ app/src/main/java/com/example/ci536groupproject/SnackBar.java
@@ -7,16 +7,18 @@ import androidx.core.content.ContextCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 
-public class SnackBar {
-    public static void snackMsg(String msg, View v) {
-        Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_LONG);
+public class UserMsg {
+    public static void snackMsg(String msg, View view) {
+        Snackbar snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
         snackbar.show();
     }
-    public static void snackMsg(String msg, View v, int textColor) {
-        Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_LONG);
+    public static void snackMsg(String msg, View view, int textColor) {
+        Snackbar snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
         View snackbarView = snackbar.getView();
         TextView textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
         textView.setTextColor(textColor);
         snackbar.show();
     }
+    public static void toastMsg(String msg, View view){
+        Toast toast = Toast.makeText(view, msg, Toast.LENGTH_LONG).show();
 }
